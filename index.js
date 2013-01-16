0b6cbc1011ba3d40fd3d299d7a72e29d105e5543
Cameron Bytheway <cameron@nujii.com>
2013-01-15 20:30:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+/*
+ * Expose acl
+ */
+module.exports.acl = require("./lib/acl");
+
+/*
+ * Expose apps
+ */
+module.exports.apps = require("./lib/apps");
+
+/*
+ * Expose config
+ */
+module.exports.config = require("./lib/config");
